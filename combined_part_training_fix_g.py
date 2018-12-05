17819d809f6f8967356e3cf96de53415f1656435
YichenQian <YichenQian@github.com>
2018-12-05 22:44:47 +0800

    update

--- combined_part_training_fix_g.py
+++ combined_part_training_fix_g.py
@@ -957,11 +957,7 @@ def main():
         avarage_cost = total_cost / t
     print(avarage_cost)
     sess.close()
-    str3 = "training_data/g_{KK}_{NN}_{MM}_{LL}_fixed_g_validation.txt".format(KK=K, NN=N, MM=M, LL=L)
-    str4 = "training_data/Q_{KK}_{NN}_{MM}_{LL}__fixed_g_validation.txt".format(KK=K, NN=N, MM=M, LL=L)
     str5 = "simulation_result/cost_{KK}_{NN}_{MM}_{LL}_fixed_g.txt".format(KK=K, NN=N, MM=M, LL=L)
-    pickle.dump(g_out,open(str3, 'wb'))
-    pickle.dump(Q_out,open(str4, 'wb'))
     pickle.dump(avarage_cost,open(str5, 'wb'))
     print ("write over")
     return avarage_cost
