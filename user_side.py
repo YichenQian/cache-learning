5028071f845bde7ca78e9078d56b0f6395bc649a
YichenQian <92yichenqian@tongji.edu.cn>
2018-12-05 15:45:35 +0800

    update

--- user_side.py
+++ user_side.py
@@ -218,7 +218,7 @@ for time in range(0, times):
                     S_kf = 0  # 0 represent not cache, 1 represent cache
                     dS_kf = 0  # 0 represent maintain, 1 represent add
                     A_k = req_old  # no request = 0
-                    S_kf1 = 1  # 0 represent not cache, 1 represent cache
+                    S_kf1 = 0  # 0 represent not cache, 1 represent cache
                     A_k1 = req_new  # no request = 0
                     num = len(maintain_n)
                     Q_next = np.zeros(num)
@@ -234,9 +234,9 @@ for time in range(0, times):
                 for j in range(N):
                     S_kf = (j + 1 in C_old_all[i, :]) + 0
                     #g[j, A_k] = min(Q_new[S_kf0, :, j, A_k0]) + Q_new[S_kf, S_kf, j, A_k].copy() - phi[j]
-                    r_c1 = (1 - (req_old == j) * (req_old != 0))
-                    phi0 = 1.0 / K / N * ((1 + P_n) ** target_fun + ((req_old not in C_E) + (P_n not in C_E)) ** target_fun)
-                    phi1 = 1.0 / K / N * ((r_c1 + P_n) ** target_fun + ((req_old not in C_E) + (P_n not in C_E)) ** target_fun)
+                    r_c1 = (1 - (req_new == j) * (req_new != 0))
+                    phi0 = 1.0 / K / N * ((1 + P_n) ** target_fun + ((req_new not in C_E) + (P_n not in C_E)) ** target_fun)
+                    phi1 = 1.0 / K / N * ((r_c1 + P_n) ** target_fun + ((req_new not in C_E) + (P_n not in C_E)) ** target_fun)
                     g[j, A_k] = Q_new[1, 1, j, A_k].copy() - Q_new[0, 0, j, A_k].copy() - phi1 + phi0
                     #g[j, A_k] = Q_new[1, 1, j, A_k].copy() + Q_new[0, 0, j, A_k].copy() - phi[j]
                 
